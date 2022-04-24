227b7906316287a235815cf6125b256aa8d538a7
mayurigupta01 <mayuri.gupta01>
2022-04-24 09:51:16 -0700

    agriDB

---
+++ app/src/main/java/com/example/hackathonchellenge/AgricultureModel.java
@@ -0,0 +1,52 @@
+package com.example.hackathonchellenge;
+
+public class AgricultureModel {
+
+    public String year;
+    public String PerAFFGDPChina;
+    public String PerAFFGDPIndia;
+    public String PerAFFGDPUSA;
+    public String ManufacturingChina;
+    public String ManufacturingIndia;
+    public String ManufacturingUSA;
+    public String AnnaulAFFChina;
+    public String AnnaulAFFIndia;
+    public String AnnaulAFFUSA;
+    public String FertilizerConsumptionChina;
+    public String FertilizerConsumptionIndia;
+    public String FertilizerConsumptionUSA;
+    public String FertilizerPerConsumptionChina;
+    public String FertilizerPerConsumptionIndia;
+    public String FertilizerPerConsumptionUSA;
+
+
+
+    public AgricultureModel(String year, String PerAFFGDPChina, String PerAFFGDPIndia,
+                            String PerAFFGDPUSA, String ManufacturingChina, String ManufacturingIndia,
+                            String ManufacturingUSA, String AnnaulAFFChina, String AnnaulAFFIndia,
+                            String AnnaulAFFUSA, String FertilizerConsumptionChina, String FertilizerConsumptionIndia,
+                            String FertilizerConsumptionUSA, String FertilizerPerConsumptionChina,
+                            String FertilizerPerConsumptionIndia, String FertilizerPerConsumptionUSA) {
+        this.year = year;
+        this.PerAFFGDPChina = PerAFFGDPChina;
+        this.PerAFFGDPIndia = PerAFFGDPIndia;
+        this.PerAFFGDPUSA = PerAFFGDPUSA;
+        this.ManufacturingChina = ManufacturingChina;
+        this.ManufacturingIndia = ManufacturingIndia;
+        this.ManufacturingUSA = ManufacturingUSA;
+        this.AnnaulAFFChina = AnnaulAFFChina;
+        this.AnnaulAFFIndia = AnnaulAFFIndia;
+        this.AnnaulAFFUSA = AnnaulAFFUSA;
+        this.FertilizerConsumptionChina = FertilizerConsumptionChina;
+        this.FertilizerConsumptionIndia = FertilizerConsumptionIndia;
+        this.FertilizerConsumptionUSA = FertilizerConsumptionUSA;
+        this.FertilizerPerConsumptionChina = FertilizerPerConsumptionChina;
+        this.FertilizerPerConsumptionIndia = FertilizerPerConsumptionIndia;
+        this.FertilizerPerConsumptionUSA = FertilizerPerConsumptionUSA;
+
+    }
+
+
+
+}
+
--- app/src/main/java/com/example/hackathonchellenge/SQLhelper.java
+++ app/src/main/java/com/example/hackathonchellenge/SQLhelper.java
@@ -14,6 +14,7 @@ public class SQLhelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "hackathon";
     private static final int DB_VERSION = 1;
     private static final String TABLE_NAME = "Macroeconomic";
+    private static  final String TABLE_NAME_AGRI = "Agriculture";
     //define columns Macroeconomic table.
     private static final String YEAR_COL = "Year";
     private static final String PER_ANNUAL_GDP_INDIA_COL = "PerAnnualGDPGrowthIndia";
@@ -31,14 +32,22 @@ public class SQLhelper extends SQLiteOpenHelper {
     private static final String FDI_OUTFLOW_DOLLAR_BOP_CHINA_COL = "FDIOutflowDollarBOPChina";
     private static final String FDI_OUTFLOW_DOLLAR_BOP_INDIA_COL = "FDIOutflowDollarBOPIndia";
     private static final String FDI_OUTFLOW_DOLLAR_BOP_USA_COL = "FDIOutflowDollarBOPUSA";
-    private static final String AGRICULTURE_FORESTRY_AND_FISHING_INDIA_PERCENT_GROWTH = "AgricultureForestryAndFishingIndia";
-    private static final String AGRICULTURE_FORESTRY_AND_FISHING_CHINA = "AgricultureForestryAndFishingChina";
-    private static final String AGRICULTURE_FORESTRY_AND_FISHING_USA = "AgricultureForestryAndFishingUSA";
-    private static final String MANUFACTURING_CHINA = "ManufacturingChina";
-    private static final String MANUFACTURING_INDIA = "ManufacturingIndia";
-    private static final String MANUFACTURING_USA = "ManufacturingUSA";
-
-
+    private static final String AGRI_YEAR = "Year";
+    private static final String PerAFFGDPChina = "PerAFFGDPChina";
+    private static final String PerAFFGDPIndia = "PerAFFGDPIndia";
+    private static final String PerAFFGDPUSA = "PerAFFGDPUSA";
+    private static final String ManufacturingChina = "ManufacturingChina";
+    private static final String ManufacturingIndia = "ManufacturingIndia";
+    private static final String ManufacturingUSA = "ManufacturingUSA";
+    private static final String AnnaulAFFChina = "AnnaulAFFChina";
+    private static final String AnnaulAFFIndia = "AnnaulAFFIndia";
+    private static final String AnnaulAFFUSA = "AnnaulAFFUSA";
+    private static final String FertilizerConsumptionChina = "FertilizerConsumptionChina";
+    private static final String FertilizerConsumptionIndia = "FertilizerConsumptionIndia";
+    private static final String FertilizerConsumptionUSA = "FertilizerConsumptionUSA";
+    private static final String FertilizerPerConsumptionChina = "FertilizerPerConsumptionChina";
+    private static final String FertilizerPerConsumptionIndia = "FertilizerPerConsumptionIndia";
+    private static final String FertilizerPerConsumptionUSA = "FertilizerPerConsumptionUSA";
 
 
     public SQLhelper(Context context) {
@@ -73,13 +82,24 @@ public class SQLhelper extends SQLiteOpenHelper {
                     + FDI_OUTFLOW_DOLLAR_BOP_USA_COL + " TEXT)";
 
             // Agriculture Table
-//
-//            String query = "CREATE TABLE " + TABLE_NAME + " ("
-//                    YEAR_COL + " TEXT,"
-//                    + AGRICULTURE_FORESTRY_AND_FISHING_CHINA + "TEXT,"
-//                            + AGRICULTURE_FORESTRY_AND_FISHING_CHINA + "TEXT,"
 
-
+        String agriQuery = "CREATE TABLE " + TABLE_NAME_AGRI + " ("
+                + AGRI_YEAR + " TEXT , "
+                + PerAFFGDPChina + " TEXT,"
+                + PerAFFGDPIndia + " TEXT,"
+                + PerAFFGDPUSA + " TEXT,"
+                + ManufacturingChina + " TEXT,"
+                + ManufacturingIndia + " TEXT,"
+                + ManufacturingUSA + " TEXT,"
+                + AnnaulAFFChina + " TEXT,"
+                + AnnaulAFFIndia + " TEXT,"
+                + AnnaulAFFUSA + " TEXT,"
+                + FertilizerConsumptionChina + " TEXT,"
+                + FertilizerConsumptionIndia + " TEXT,"
+                + FertilizerConsumptionUSA + " TEXT,"
+                + FertilizerPerConsumptionChina + " TEXT,"
+                + FertilizerPerConsumptionIndia + " TEXT,"
+                + FertilizerPerConsumptionUSA + " TEXT)";
 
         // at last we are calling a exec sql
             // method to execute above sql query
@@ -123,6 +143,43 @@ public class SQLhelper extends SQLiteOpenHelper {
         db.close();
     }
 
+
+
+
+    public void addAgriDataoDB(String[] inValues) {
+
+        // on below line we are creating a variable for
+        // our sqlite database and calling writable method as we update data to db.
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // creating a variable for content values.
+        ContentValues values = new ContentValues();
+
+        // on below line we are passing all value along with its key and value pair.
+        values.put(YEAR_COL, inValues[0]);
+        values.put(PerAFFGDPChina, addNullValues(inValues[1]));
+        values.put(PerAFFGDPIndia, addNullValues(inValues[2]));
+        values.put(PerAFFGDPUSA, addNullValues(inValues[3]));
+        values.put(ManufacturingChina, addNullValues(inValues[4]));
+        values.put(ManufacturingIndia, addNullValues(inValues[5]));
+        values.put(ManufacturingUSA, addNullValues(inValues[6]));
+        values.put(AnnaulAFFChina, addNullValues(inValues[7]));
+        values.put(AnnaulAFFIndia, addNullValues(inValues[8]));
+        values.put(AnnaulAFFUSA, addNullValues(inValues[9]));
+        values.put(FertilizerConsumptionChina, addNullValues(inValues[10]));
+        values.put(FertilizerConsumptionIndia, addNullValues(inValues[11]));
+        values.put(FertilizerConsumptionUSA, addNullValues(inValues[12]));
+        values.put(FertilizerPerConsumptionChina, addNullValues(inValues[13]));
+        values.put(FertilizerPerConsumptionIndia, addNullValues(inValues[14]));
+        values.put(FertilizerPerConsumptionUSA, addNullValues(inValues[15]));
+        // after adding all values we are passing content values to our table.
+        db.insert(TABLE_NAME, null, values);
+
+        // at last we are closing our
+        // database after adding database.
+        db.close();
+    }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // this method is called to check if the table exists already.
@@ -131,11 +188,12 @@ public class SQLhelper extends SQLiteOpenHelper {
     }
 
     private String addNullValues(String columnValue){
-        if (columnValue.isEmpty()) {
+        if (columnValue.isEmpty() || columnValue.startsWith("#")) {
             return "0.0";
         }
         return columnValue;
     }
+
     public ArrayList<MacroeconomicsModel> readMacroeconomicsData(){
         SQLiteDatabase db = this.getReadableDatabase();
         System.out.println(db);
@@ -170,6 +228,45 @@ public class SQLhelper extends SQLiteOpenHelper {
         System.out.println(courseModalArrayList);
         cursorCourses.close();
         return courseModalArrayList;
+
+    }
+
+
+
+    public ArrayList<AgricultureModel> readAgricultureData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        System.out.println(db);
+        Cursor cursorCourses = db.rawQuery("SELECT * FROM " + TABLE_NAME_AGRI
+                , null);
+        // on below line we are creating a new array list.
+        ArrayList<AgricultureModel> AgricultureModalArrayList = new ArrayList<>();
+        // moving our cursor to first position.
+        if (cursorCourses.moveToFirst()) {
+            do {
+                AgricultureModalArrayList.add(new AgricultureModel(cursorCourses.getString(0),
+                        cursorCourses.getString(1),
+                        cursorCourses.getString(2),
+                        cursorCourses.getString(3),
+                        cursorCourses.getString(4),
+                        cursorCourses.getString(5),
+                        cursorCourses.getString(6),
+                        cursorCourses.getString(7),
+                        cursorCourses.getString(8),
+                        cursorCourses.getString(9),
+                        cursorCourses.getString(10),
+                        cursorCourses.getString(11),
+                        cursorCourses.getString(12),
+                        cursorCourses.getString(13),
+                        cursorCourses.getString(14),
+                        cursorCourses.getString(15)));
+            }
+            while (cursorCourses.moveToNext()) ;
+        }
+        // at last closing our cursor
+        // and returning our array list.
+        System.out.println(AgricultureModalArrayList);
+        cursorCourses.close();
+        return AgricultureModalArrayList;
 
     }
 }
